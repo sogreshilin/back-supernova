640154f08a670a9ee59325e5e4de8f089af1e4af
Alexander Sogreshilin <sogreshilin@yandex-team.ru>
2019-05-18 21:24:56 +0700

    SUPERNOVA-3: add favourite to person

--- src/main/java/ru/nsu/controller/person/FavouriteDto.java
+++ src/main/java/ru/nsu/controller/person/FavouriteDto.java
@@ -1,9 +1,15 @@
 package ru.nsu.controller.person;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Value;
 import ru.nsu.entity.enums.EventType;
 
 @Value
 public class FavouriteDto {
     private final EventType type;
+
+    @JsonCreator
+    public FavouriteDto(EventType type) {
+        this.type = type;
+    }
 }
--- src/main/resources/liquibase/migrations/01-base.sql
+++ src/main/resources/liquibase/migrations/01-base.sql
@@ -3,60 +3,60 @@
 --changeset sogreshilin:supernova-01-creator-table
 create table creator
 (
-    id   bigserial primary key,
-    name text not null,
-    type text not null
+  id   bigserial primary key,
+  name text not null,
+  type text not null
 );
 
 --changeset sogreshilin:supernova-01-person-table
 create table person
 (
-    id         bigint primary key,
-    vk_id      bigint,
-    last_name  text not null,
-    first_name text not null,
-    city       text not null
+  id         bigserial primary key,
+  vk_id      text not null,
+  last_name  text not null,
+  first_name text not null
 );
 
 create table location
 (
-    id        bigserial primary key,
-    latitude  double precision,
-    longitude double precision,
-    city      text not null,
-    address   text not null
+  id        bigserial primary key,
+  latitude  double precision,
+  longitude double precision,
+  city      text not null,
+  address   text not null
 );
 
 --changeset sogreshilin:supernova-01-event-table
 create table event
 (
-    id            bigserial primary key,
-    title         text                     not null,
-    description   text                     not null,
-    creator_id    bigint                   not null references creator,
-    rating        double precision,
-    location_id   bigint references location,
-    from_datetime timestamp with time zone not null,
-    to_datetime   timestamp with time zone not null,
-    site_url      text,
-    phone         text,
-    email         text
+  id            bigserial primary key,
+  title         text                     not null,
+  description   text                     not null,
+  creator_id    bigint                   not null references creator,
+  rating        double precision,
+  location_id   bigint references location,
+  from_datetime timestamp with time zone not null,
+  to_datetime   timestamp with time zone not null,
+  site_url      text,
+  phone         text,
+  email         text
 );
 
 --changeset sogreshilin:supernova-01-event-type-table
 create table event_type
 (
-    event_id bigint not null references event,
-    type     text   not null
+  event_id bigint not null references event,
+  type     text   not null,
+  unique (event_id, type)
 );
 
 --changeset sogreshilin:supernova-01-event-member-table
 create table event_person
 (
-    event_id  bigint references event,
-    person_id bigint references person,
-    weight    double precision not null,
-    primary key (event_id, person_id)
+  event_id  bigint references event,
+  person_id bigint references person,
+  weight    double precision not null,
+  primary key (event_id, person_id)
 );
 
 comment on column event_person.weight is 'Насколько данному пользователю подходит данное событие. Используется в рекомендательной системе';
@@ -64,13 +64,13 @@ comment on column event_person.weight is 'Насколько данному по
 --changeset sogreshilin:supernova-01-event-image-table
 create table event_image
 (
-    event_id  bigint not null references event,
-    image_url text   not null
+  event_id  bigint not null references event,
+  image_url text   not null
 );
 
 --changeset miliaev:supernova-04-person-favourites-table
 create table person_favourites
 (
-    person_id bigint not null references person,
-    type      text   not null
+  person_id bigint not null references person,
+  type      text   not null
 );
